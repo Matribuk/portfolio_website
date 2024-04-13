aef43e16283bdd13e5975608ab6f1537a2d80a5c
Matribuk <antonin.leprest@epitech.eu>
2024-04-13 14:39:02 +0200

    Update next.config.js
    Signed-off-by: Matribuk <antonin.leprest@epitech.eu>

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-
-  basePath: '/portfolio_website',
-  assetPrefix: '/portfolio_website/',
-
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
 }
 
 module.exports = nextConfig
